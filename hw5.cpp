373f77d5b253f6ccfa741d6d15861d0040d4a8cc
BRODY MILLS <bdm127@txstate.edu>
2020-10-05 04:59:01 +0000

    hyrft

--- hw5.cpp
+++ hw5.cpp
@@ -16,7 +16,7 @@ void BubbleSort(std::vector<std::string> &vec){
    
   for (int i = 0; i < vec.size()-1; i++) {
     
-    for (int j = 0; j < vec.size()-(i+1); j++)
+    for (int j = 0; j < (vec.size()-(i+1)); j++)
     if (vec[j] > vec[j+1]){
       StringSwap(vec[j],vec[j+1]);
     }
